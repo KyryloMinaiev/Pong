ba9d9c936d2d163bc4f918f5e05ad25c59d63b53
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-08-20 13:54:14 +0300

    Moved container to Core folder

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,6 @@
 ﻿#include "Game.h"
 
 #include <SFML\Graphics.hpp>
-#include "DIContainer/Container.h"
 #include "MainMenuModule/MainMenuModule.h"
 
 Game::Game(sf::RenderWindow* window) : m_window(window), m_container(std::make_unique<Container>())
