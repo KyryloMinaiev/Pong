daf4d44f1ff0709ff35e4bfbb91eb2a8046bd3db
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-08-22 15:55:59 +0300

    Added framelimit and window closing event

--- src/Pong.cpp
+++ src/Pong.cpp
@@ -5,8 +5,9 @@
 
 int main()
 {
-    sf::VideoMode videoMode = sf::VideoMode::getDesktopMode();
-    sf::RenderWindow window(videoMode, "Pong", sf::State::Fullscreen);
+    sf::VideoMode videoMode = sf::VideoMode(sf::Vector2u(1080, 720));
+    sf::RenderWindow window(videoMode, "Pong", sf::Style::Titlebar | sf::Style::Close, sf::State::Windowed);
+    window.setFramerateLimit(60);
     std::unique_ptr<Game> game = std::make_unique<Game>(&window);
     sf::Clock clock;
     while (window.isOpen())
@@ -14,6 +15,18 @@ int main()
         window.clear();
         float deltaTime = clock.restart().asSeconds();
         game->update(deltaTime);
+
+        float fps = 1.0f / deltaTime;
+        window.setTitle("Pong - " + std::to_string(fps));
+        window.display();
+
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
     }
     
     return 0;
