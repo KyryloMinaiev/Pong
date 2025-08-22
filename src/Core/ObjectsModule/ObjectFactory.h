c1e0b2ed25d3edbff00823ab39fcfc8b5e1f824e
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-08-22 15:33:08 +0300

    Added objects creation

--- src/Core/ObjectsModule/ObjectFactory.h
+++ src/Core/ObjectsModule/ObjectFactory.h
@@ -1,4 +1,9 @@
 ﻿#pragma once
+#include <memory>
+#include <type_traits>
+
+#include "Object.h"
+#include "ObjectsContainer.h"
 
 class ObjectsContainer;
 
@@ -9,6 +14,28 @@ public:
     virtual ~ObjectFactory() = default;
     void inject(ObjectsContainer* objects_container);
 
+    template<typename TObject>
+    requires std::is_base_of_v<TObject, Object>
+    TObject* createObjectOfType();
+
+    template<typename TObject>
+    requires std::is_base_of_v<TObject, Object>
+    TObject* createObjectOfType(const std::string& name);
 private:
     ObjectsContainer* m_objects_container;
 };
+
+template <typename TObject> requires std::is_base_of_v<TObject, Object>
+TObject* ObjectFactory::createObjectOfType()
+{
+    return createObjectOfType<TObject>("New Object");   
+}
+
+template <typename TObject> requires std::is_base_of_v<TObject, Object>
+TObject* ObjectFactory::createObjectOfType(const std::string& name)
+{
+    auto object_ptr = std::make_shared<TObject>();
+    object_ptr->name = name;
+    m_objects_container->addObject(object_ptr);
+    return object_ptr.get();  
+}
