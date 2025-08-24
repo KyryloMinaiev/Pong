c2e040bcf27fd929cc111f802172c443ddd76063
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-08-24 20:41:25 +0300

    Added background to fps counter

--- src/DebugModule/FPSCounter.cpp
+++ src/DebugModule/FPSCounter.cpp
@@ -2,17 +2,21 @@
 
 FPSCounter::~FPSCounter() = default;
 
-void FPSCounter::inject(ITextFactory* textFactory)
+void FPSCounter::inject(ITextFactory* textFactory, IImageFactory* imageFactory)
 {
-    m_textFactory = textFactory; 
+    m_textFactory = textFactory;
+    m_imageFactory = imageFactory; 
 }
 
 void FPSCounter::initialize()
 {
+    m_background = m_imageFactory->createImage("res/textures/pixel.png");
+    m_background->setScale({45, 18});
+    m_background->setColor({20, 20, 20, 255});
     m_fpsCounterText = m_textFactory->createText("FPSCounter", TextSettings());
 }
 
 void FPSCounter::update(float deltaTime)
 {
-    m_fpsCounterText->setText("FPS: " + std::to_string(1.0f / deltaTime));
+    m_fpsCounterText->setText("FPS: " + std::to_string(static_cast<int>(1.0f / deltaTime)));
 }
